93281c3ea32a3a358b8c5a7b44a497f0dae80d9a
Maxim Karachyov <karachyov.m@gmail.com>
2022-11-21 14:57:39 +0300

    lookbook

--- src/js/lookbook.js
+++ src/js/lookbook.js
@@ -1,5 +1,42 @@
-@@include('./partials/jquery.formstyler.js')
+@@include('./partials/swiper-bundle.min.js')
 
-setTimeout(function() {
-    $('.js-select').styler();
-}, 100)
+var swiperOther= swiperOther;
+var swiperOtherInit = false;
+
+
+function swiperOtherMode() {
+    let mobile = window.matchMedia('(min-width: 0px) and (max-width: 767px)');
+    let desktop = window.matchMedia('(min-width: 768px)');
+    if (mobile.matches) {
+        if(!swiperOtherInit){
+            swiperOtherInit = true;
+            swiperOther = new Swiper('.swiper-other-collection', {
+                slidesPerView: 1.4,
+                spaceBetween:6,
+                initialSlide: 0,
+                observer: true,
+                observeParents: true,
+            });
+
+        }
+    }
+
+    else if (desktop.matches) {
+        if (swiperOther) {
+            swiperOther.destroy();
+            swiperOtherInit = false;
+        }
+    }
+}
+
+window.addEventListener('load', function() {
+    if($('.swiper-other-collection').length){
+        swiperOtherMode();
+    }
+});
+
+window.addEventListener('resize', function() {
+    if($('.swiper-other-collection').length){
+        swiperOtherMode();
+    }
+});
