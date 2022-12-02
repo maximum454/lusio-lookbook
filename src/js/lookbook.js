405cd7bd598f9553b5d9a43f0160060279aec56b
Maxim Karachyov <karachyov.m@gmail.com>
2022-12-02 15:17:35 +0300

    fix

--- src/js/lookbook.js
+++ src/js/lookbook.js
@@ -1,10 +1,7 @@
-@@include('./partials/swiper-bundle.min.js')
 @@include('./partials/remodal.js')
 
 var swiperOther = swiperOther;
 var swiperOtherInit = false;
-
-
 function swiperOtherMode() {
     let mobile = window.matchMedia('(min-width: 0px) and (max-width: 767px)');
     let desktop = window.matchMedia('(min-width: 768px)');
@@ -28,50 +25,15 @@ function swiperOtherMode() {
     }
 }
 
-var swiperItems = swiperItems;
-var swiperItemsInit = false;
-
-function swiperItemsMode() {
-    let mobile = window.matchMedia('(min-width: 0px) and (max-width: 767px)');
-    let desktop = window.matchMedia('(min-width: 768px)');
-    if (mobile.matches) {
-        if (!swiperItemsInit) {
-            swiperItemsInit = true;
-            document.querySelectorAll('.lookbook-items').forEach(n => {
-                swiperItems = new Swiper(n.querySelector('.swiper-lookbook-items'), {
-                    slidesPerView: 2.2,
-                    spaceBetween: 6,
-                    initialSlide: 0,
-                    observer: true,
-                    observeParents: true,
-
-                });
-            });
-
-        }
-    } else if (desktop.matches) {
-        if (swiperItems) {
-            swiperItems.destroy();
-            swiperItemsInit = false;
-        }
-    }
-}
-
 window.addEventListener('load', function () {
     if ($('.swiper-other-collection').length) {
         swiperOtherMode();
-    }
-    if ($('.swiper-lookbook-items').length) {
-        swiperItemsMode();
     }
 });
 
 window.addEventListener('resize', function () {
     if ($('.swiper-other-collection').length) {
         swiperOtherMode();
-    }
-    if ($('.swiper-lookbook-items').length) {
-        swiperItemsMode();
     }
 });
 
